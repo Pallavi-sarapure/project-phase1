b8ce2ac98ae957c57fb36a20c62da0c41effc119
Pallavi-sarapure <pallavisarapure1998@gmail.com>
2022-01-16 14:10:32 +0530

    project

---
+++ src/LockedMeMain.java
@@ -0,0 +1,11 @@
+
+public class LockedMeMain {
+	public static void main(String[] args) {
+		FileOperations.createMainFolderIfNotPresent("main");
+		MenuOptions.printWelcomeScreen("lockedMe","pallavi");
+		HandleOptions.handleWelcomeScreenInput();
+	}
+	}
+	
+
+
